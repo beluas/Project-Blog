484a533473b12b5ccde58e8999eb1fb94f070275
Ivan Maddaluno <ivanmaddaluno1993@gmail.com>
2020-05-22 19:40:01 +0200

    published on github pages

--- src/components/body/Body.components.js
+++ src/components/body/Body.components.js
@@ -4,48 +4,58 @@ import "./body.styles.scss";
 import ProjectTab from "../projectTab/ProjectTab.component";
 import Skills from "../skills/Skills.component";
 
-const Body = () => {
-	return (
-		<div className="body">
-			<Skills />
-			<ProjectTab
-				skills="React"
-				title="title1"
-				desc="lorem ipsum lorem abcd fgjsoe"
-				tier="1 - Beginner"
-				collection="Api"
-			/>
+class Body extends React.Component {
+	state = {
+		elementHovered: false,
+	};
 
-			<ProjectTab
-				skills="React"
-				title="title1"
-				desc="lorem ipsum lorem abcd fgjsoe"
-				tier="1 - Beginner"
-				collection="Api"
-			/>
-			<ProjectTab
-				skills="React"
-				title="title1"
-				desc="lorem ipsum lorem abcd fgjsoe"
-				tier="1 - Beginner"
-				collection="Api"
-			/>
-			<ProjectTab
-				skills="React"
-				title="title1"
-				desc="lorem ipsum lorem abcd fgjsoe"
-				tier="1 - Beginner"
-				collection="Api"
-			/>
-			<ProjectTab
-				skills="React"
-				title="title1"
-				desc="lorem ipsum lorem abcd fgjsoe"
-				tier="1 - Beginner"
-				collection="Api"
-			/>
-		</div>
-	);
-};
+	handleHover = () => {
+		this.setState({ elementHovered: true });
+	};
+
+	render() {
+		return (
+			<div className="body">
+				<Skills />
+				<ProjectTab
+					skills="React"
+					title="Title1"
+					desc="lorem ipsum lorem abcd fgjsoe"
+					tier="1 - Beginner"
+					collection="Api"
+				/>
+
+				<ProjectTab
+					skills="React"
+					title="title1"
+					desc="lorem ipsum lorem abcd fgjsoe"
+					tier="1 - Beginner"
+					collection="Api"
+				/>
+				<ProjectTab
+					skills="React"
+					title="title1"
+					desc="lorem ipsum lorem abcd fgjsoe"
+					tier="1 - Beginner"
+					collection="Api"
+				/>
+				<ProjectTab
+					skills="React"
+					title="title1"
+					desc="lorem ipsum lorem abcd fgjsoe"
+					tier="1 - Beginner"
+					collection="Api"
+				/>
+				<ProjectTab
+					skills="React"
+					title="title1"
+					desc="lorem ipsum lorem abcd fgjsoe"
+					tier="1 - Beginner"
+					collection="Api"
+				/>
+			</div>
+		);
+	}
+}
 
 export default Body;
