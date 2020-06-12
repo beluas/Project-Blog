a5ce0b0983b5e1957745e536d877166324bf7741
Ivan Maddaluno <ivanmaddaluno1993@gmail.com>
2020-06-12 12:56:19 +0200

    project detail updated

--- src/projects.data.js
+++ src/projects.data.js
@@ -14,7 +14,6 @@ export const projects = [
 		desc:
 			"App created with React, Firebase to store notes, React-Quill for the editor and Material-UI to style components.",
 		skills: "React, Material-UI, Firebase",
-		tier: "n.8 - Intermediate",
 		collection: "App Cloned",
 		link: "https://beluas.github.io/7-Evernote-clone/",
 		greatest: true,
@@ -24,7 +23,6 @@ export const projects = [
 		desc:
 			"Website created with React using Redux-Saga to handle the api request to the recipe api database.",
 		skills: "React, Redux, Redux-saga",
-		tier: "n.2 - Beginner",
 		collection: "Api",
 		link: "https://beluas.github.io/2-recipeat/",
 	},
@@ -32,7 +30,6 @@ export const projects = [
 		title: "Youtube Clone",
 		desc: "Youtube Clone created using React",
 		skills: "React, CSS3",
-		tier: "n.12 - Beginner",
 		collection: "App Cloned",
 		link: "https://react-jyaf1u.stackblitz.io/",
 	},
@@ -41,7 +38,6 @@ export const projects = [
 		desc:
 			"A couple of amazing animation on hover for buttons using CSS3 animation with SASS",
 		skills: "CSS3, SASS",
-		tier: "n.9 - Intermediate",
 		collection: "Animations",
 		link: "https://beluas.github.io/8-Button-Animations/",
 	},
@@ -50,7 +46,6 @@ export const projects = [
 		desc:
 			"Real Estate project using a Json file to fetch the data with Redux-saga. Searching through apartments for different parameters is available. To Implement : Integration with Google Maps Api / Pagination for all apartments on the website (Around 100 items)",
 		skills: "React, Redux, Redux-Saga, SASS",
-		tier: "n.7 - Intermediate",
 		collection: "Websites",
 		link: "https://beluas.github.io/6-Real-Estate/",
 		greatest: true,
@@ -61,7 +56,6 @@ export const projects = [
 		desc:
 			"TO COMPLETE. 100 Algorithms challenge. Algorithms written using Typescript and Javascript",
 		skills: "Javascript, Typescript",
-		tier: "n.6 - Intermediate",
 		collection: "Algorithms",
 		link: "https://github.com/beluas/100-Algorithm-challenge-TS",
 		greatest: true,
@@ -71,7 +65,6 @@ export const projects = [
 		desc:
 			"A couple of amazing animation on hover for HTML Elements using CSS3 animation with SASS",
 		skills: "CSS3, SASS",
-		tier: "n.10 - Intermediate",
 		collection: "Animations",
 		link: "https://beluas.github.io/9-Element-Animations/",
 	},
@@ -80,7 +73,6 @@ export const projects = [
 		desc:
 			"Website created using Bootstrap and AOS library to handle animations ",
 		skills: "Bootstrap, Javascript, SASS",
-		tier: "n.5 - Beginner",
 		collection: "Websites",
 		link: " https://beluas.github.io/5-Minimal-Portfolio/",
 	},
@@ -88,7 +80,6 @@ export const projects = [
 		title: "Nature Website",
 		desc: "A simple One Page template made with bootstrap ",
 		skills: "Bootstrap, Javascript, SASS",
-		tier: "n.4 - Beginner",
 		collection: "Websites",
 		link: " https://beluas.github.io/4-Bootstrap-Website/",
 	},
@@ -97,7 +88,6 @@ export const projects = [
 		desc:
 			"A couple of amazing animation on hover for Icons using CSS3 animation with SASS",
 		skills: "CSS3, SASS",
-		tier: "n.11 - Intermediate",
 		collection: "Websites",
 		link: "https://beluas.github.io/10-Icons-Animations/",
 	},
@@ -106,7 +96,6 @@ export const projects = [
 		desc:
 			"Infinite Scroll Images component with communication with Server to handle Api requests. react-infinite-scrolling-component used to handle the infinite feed ",
 		skills: "React, Express",
-		tier: "n.3 - Beginner",
 		collection: "Full-Stack",
 		link: "https://github.com/beluas/3-Infinite-Scroll-Images",
 	},
