b4be5c91d76f170781c087e9c9b2c88469b4f446
Ivan Maddaluno <ivanmaddaluno1993@gmail.com>
2020-06-12 12:27:22 +0200

    E-commerce website added

--- src/projects.data.js
+++ src/projects.data.js
@@ -1,5 +1,15 @@
 export const projects = [
 	{
+		title: "E-commerce Clothing",
+		desc:
+			"Fully functional E-commerce Website using React, Redux, Reselect, Stripe-api to handle payments, Firebase to handle the registration and the sign-in of the users, redux-persist to actually store data in localStorage to not lose what you did on the website and React Router DOM to actually handle the Routes. styling created in SASS",
+		skills:
+			"React, Redux, Redux-persist, React-router-dom Reselect, Firebase, SASS, Stripe-Api, React Router",
+		collection: "Professional Website",
+		link: "https://beluacode-ecommerce.herokuapp.com/",
+		greatest: true,
+	},
+	{
 		title: "Evernote Clone",
 		desc:
 			"App created with React, Firebase to store notes, React-Quill for the editor and Material-UI to style components.",
